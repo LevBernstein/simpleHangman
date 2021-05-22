a215fc727dcf5b6b5fd02913d48f005bb3127928
LevBernstein <lev2000@me.com>
2021-05-22 14:45:36 -0400

    autofill if one blank remaining

--- hangman.py
+++ hangman.py
@@ -21,10 +21,14 @@ def setup():
     words = []
     progress = []
     wordLen = int(input("What is the length of your word?\n"))
-    with open("words.txt", "r") as wordList:
-        for line in wordList.read().splitlines():
-            if wordLen == len(line):
-                words.append(line.upper())
+    try:
+        with open("words.txt", "r") as wordList:
+            for line in wordList.read().splitlines():
+                if wordLen == len(line):
+                    words.append(line.upper())
+    except:
+        print("Error! Could not read words.txt!")
+        return False
     if len(words) < 1:
         print("I have no words of that length in my dictionary. Sorry.")
         return False
@@ -64,7 +68,7 @@ def guess():
                 searching = False
                 break
     return guess
-    
+
 def main():
     global progress, words, guesses
     valid = setup()
@@ -85,30 +89,35 @@ def main():
                 response = True
         if correct.startswith("y"):
             newWords = []
-            valid = False
             print("I'm so smart.")
-            while not valid:
-                valid = True
-                positions = input("Please enter the positions in which this letter appears, starting at 1 on the left, separated by a space.\n").split(" ")
-                for position in positions:
-                    if progress[int(position) - 1] != "_":
-                        valid = False
-                        break
-                if not valid:
-                    print("There's already a letter in at least one of the positions you gave!")
-            for position in positions:
-                progress[int(position) - 1] = letter
-            for word in words:
-                if letter in word:
+            if progress.count("_") > 1:
+                valid = False
+                while not valid:
                     valid = True
-                    for i in range (len(progress)):
-                        if progress[i] != "_":
-                            if progress[i] != word[i]:
-                                valid = False
-                                break
-                    if valid:
-                        newWords.append(word)
-            words = newWords
+                    positions = input("Please enter the positions in which this letter appears, starting at 1 on the left, separated by a space.\n").split(" ")
+                    for position in positions:
+                        if progress[int(position) - 1] != "_":
+                            valid = False
+                            break
+                    if not valid:
+                        print("There's already a letter in at least one of the positions you gave!")
+                for position in positions:
+                    progress[int(position) - 1] = letter
+                for word in words:
+                    if letter in word:
+                        valid = True
+                        for i in range (len(progress)):
+                            if progress[i] != "_":
+                                if progress[i] != word[i]:
+                                    valid = False
+                                    break
+                        if valid:
+                            newWords.append(word)
+                words = newWords
+            else: # if only one blank left, autofill
+                for i in range(len(progress)):
+                    if progress[i] == "_":
+                        progress[i] = letter
         else:
             print("Oops. Well, nobody's perfect.")
             guesses += 1
